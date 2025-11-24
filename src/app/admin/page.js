e9a5cbda6f148cc12bc3a8779ef86e792a50345a
master-ginger <bhavanapillai.100@gmail.com>
2025-11-24 18:33:59 +0530

    Added preset for cloudinart

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -26,7 +26,7 @@ export default function Admin() {
     const formData = new FormData();
     formData.append("file", file);
 
-    formData.append("upload_preset", "your_upload_preset");
+    formData.append("upload_preset", "Portfolio-Products");
 
   const res = await fetch("https://api.cloudinary.com/v1_1/djgn6hpxm/image/upload", {
     method: "POST",
